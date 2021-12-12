48aeb3b3e2f52731c4ff685e05c2198d17f1826c
francis <francisaboutlee@gmail.com>
2021-12-12 17:47:21 +0800

    update

---
+++ python_code/textbooks/automate_the_boring_stuff_with_python/coinToss.py
@@ -0,0 +1,15 @@
+# import random
+
+# guess = ''
+# while guess not in ('h', 't'):
+# 	print("guess the coin toss. (enter 'h' or 't')")
+# 	guess  = input()
+# toss = random.randint(0, 1)
+# if toss == int(guess):
+# 	print('you got it')
+# else:
+# 	print('Nope. guess again')
+# 	if toss == int(guess):
+# 		print('You got it')
+# 	else:
+# 		print('nope, you are really bad at his game.')
