0fd7b0844c752f473405dcff50ecd597b8360361
Francis <68459730+francisliujia@users.noreply.github.com>
2021-12-30 15:38:41 +0800

    Add files via upload

---
+++ python_code/textbooks/create_folder.py
@@ -0,0 +1,17 @@
+__author__ = 'francisaboutlee@gmail.com'
+import os 
+
+current_path = os.path.abspath('.')
+chapter_ls = ['chapter '+ str(i) for i in range(1, 22)] 
+
+# chapter_ls= list()
+# for i in range(1, 22):
+# 	chapter = 'chapter' + str(i)
+# 	chapter_ls.append(chapter)
+
+
+print(chapter_ls)
+
+for dir_path in chapter_ls: 
+	path = os.path.join(current_path, dir_path)
+	os.makedirs(path)
