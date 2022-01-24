634d91875f3885c980e6974129c8f44242c58c93
francis <francisaboutlee@gmail.com>
2022-01-24 20:50:51 +0800

    update

--- python_code/textbooks/python_cookbook/chapter_9_classes/my_restaurant.py
+++
@@ -1,5 +0,0 @@
-from restaurant import Restaurant
-
-love_birds = Restaurant('love bird', 'fried butt')
-love_birds.describe_restaurant()
-love_birds.open_restaurant()
--- python_code/textbooks/python_cookbook/testing_your_code/city_functions.py
+++
@@ -1,6 +0,0 @@
-def city_country(city, country, population=0):
-     output_string = f"{city.title()}, {country.title()}"
-     if population:
-         output_string += f" -population: {population}"
-     return  output_string
-
